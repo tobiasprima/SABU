69105ccf205356184e872ba0215e8c5a0071b15d
Tobias Prima <primatjahja@gmail.com>
2025-01-17 19:26:07 +0800

    feat: restaurant reposiitory get meals by restaurant id

--- restaurant-service/repository/restaurant_repository.go
+++ restaurant-service/repository/restaurant_repository.go
@@ -5,6 +5,7 @@ import (
 	"sabu-restaurant-service/config"
 	"sabu-restaurant-service/models"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"gorm.io/gorm"
 )
@@ -28,4 +29,19 @@ func (r *RestaurantRepository) CreateRestaurant(restaurant *models.Restaurant) e
 func (r *RestaurantRepository) AddMeal(ctx context.Context, meals *models.Meal) error {
 	_, err := r.MealsCollection.InsertOne(ctx, meals)
 	return err
+}
+
+func (r *RestaurantRepository) GetMealsByRestaurantID(ctx context.Context, restaurantID string) ([]models.Meal, error) {
+	cursor, err := r.MealsCollection.Find(ctx, bson.M{"restaurant_id": restaurantID})
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(ctx)
+
+	var meals []models.Meal
+	if err := cursor.All(ctx, &meals); err != nil {
+		return nil, err
+	}
+
+	return meals, nil
 }
