49103b1f71edf7af97d10bec4a523b3a53c222ae
KuR0zZ <ferdinandiphone6@gmail.com>
2025-01-21 19:56:17 +0700

    feat: restaurant service grpc for GetMealByID & DeductMealStock

--- restaurant-service/handlers/restaurant_grpc_handler.go
+++ restaurant-service/handlers/restaurant_grpc_handler.go
@@ -6,23 +6,28 @@ import (
 	"sabu-restaurant-service/proto/pb"
 	"sabu-restaurant-service/repository"
 	"sync"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type RestaurantGRpcHandler struct {
 	pb.UnimplementedRestaurantServiceServer
-	RestaurantRepo		*repository.RestaurantRepository
-	preparedRestaurants	map[string]*models.Restaurant
-	mu					sync.Mutex
+	RestaurantRepo      *repository.RestaurantRepository
+	preparedRestaurants map[string]*models.Restaurant
+	preparedMeals       map[string]*models.Meal
+	mu                  sync.Mutex
 }
 
 func NewRestaurantGRpcHandler() *RestaurantGRpcHandler {
 	return &RestaurantGRpcHandler{
-		RestaurantRepo: repository.NewRestaurantRepository(),
+		RestaurantRepo:      repository.NewRestaurantRepository(),
 		preparedRestaurants: make(map[string]*models.Restaurant),
+		preparedMeals:       make(map[string]*models.Meal),
 	}
 }
 
-func (h *RestaurantGRpcHandler) PrepareRestaurant(ctx context.Context, req *pb.PrepareRestaurantRequest) (*pb.PrepareRestaurantResponse, error){
+func (h *RestaurantGRpcHandler) PrepareRestaurant(ctx context.Context, req *pb.PrepareRestaurantRequest) (*pb.PrepareRestaurantResponse, error) {
 	h.mu.Lock()
 	defer h.mu.Unlock()
 
@@ -46,7 +51,7 @@ func (h *RestaurantGRpcHandler) PrepareRestaurant(ctx context.Context, req *pb.P
 	}, nil
 }
 
-func (h *RestaurantGRpcHandler) CommitRestaurant(ctx context.Context, req *pb.CommitRestaurantRequest) (*pb.CommitRestaurantResponse, error){
+func (h *RestaurantGRpcHandler) CommitRestaurant(ctx context.Context, req *pb.CommitRestaurantRequest) (*pb.CommitRestaurantResponse, error) {
 	h.mu.Lock()
 	defer h.mu.Unlock()
 
@@ -88,5 +93,94 @@ func (h *RestaurantGRpcHandler) RollbackRestaurant(ctx context.Context, req *pb.
 	return &pb.RollbackRestaurantResponse{
 		Success: false,
 		Message: "No prepared restaurant found for this user",
+	}, nil
+}
+
+func (h *RestaurantGRpcHandler) GetMealByID(ctx context.Context, req *pb.MealID) (*pb.GetMealByIDResponse, error) {
+	mealTmp, err := h.RestaurantRepo.GetMealByID(ctx, req.Id)
+	if err != nil {
+		return nil, err
+	}
+
+	if mealTmp == nil {
+		return nil, status.Error(codes.NotFound, "Meals not found")
+	}
+
+	meal := &pb.GetMealByIDResponse{
+		Id:           mealTmp.ID,
+		RestaurantId: mealTmp.RestaurantID,
+		Name:         mealTmp.Name,
+		Description:  mealTmp.Description,
+		Price:        float32(mealTmp.Price),
+		Stock:        uint32(mealTmp.Stock),
+	}
+
+	return meal, nil
+}
+
+func (h *RestaurantGRpcHandler) PrepareDeductMealStock(ctx context.Context, req *pb.PrepareDeductMealStockRequest) (*pb.PrepareDeductMealStockResponse, error) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+
+	if _, exists := h.preparedMeals[req.DonationId]; exists {
+		return &pb.PrepareDeductMealStockResponse{
+			Success: false,
+			Message: "Meal preparation already exists for this donation",
+		}, nil
+	}
+
+	h.preparedMeals[req.DonationId] = &models.Meal{
+		ID:    req.MealId,
+		Stock: int(req.Quantity),
+	}
+
+	return &pb.PrepareDeductMealStockResponse{
+		Success: true,
+		Message: "Meal prepared successfully",
+	}, nil
+}
+
+func (h *RestaurantGRpcHandler) CommitDeductMealStock(ctx context.Context, req *pb.CommitDeductMealStockRequest) (*pb.CommitDeductMealStockResponse, error) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+
+	meal, exists := h.preparedMeals[req.DonationId]
+	if !exists {
+		return &pb.CommitDeductMealStockResponse{
+			Success: false,
+			Message: "No Prepared meal found for this donation",
+		}, nil
+	}
+
+	if err := h.RestaurantRepo.DeductMealStock(ctx, meal); err != nil {
+		return &pb.CommitDeductMealStockResponse{
+			Success: false,
+			Message: "Failed to commit meal: " + err.Error(),
+		}, nil
+	}
+
+	delete(h.preparedMeals, req.DonationId)
+
+	return &pb.CommitDeductMealStockResponse{
+		Success: true,
+		Message: "Meal committed successfully",
+	}, nil
+}
+
+func (h *RestaurantGRpcHandler) RollbackDeductMealStock(ctx context.Context, req *pb.RollbackDeductMealStockRequest) (*pb.RollbackDeductMealStockResponse, error) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+
+	if _, exists := h.preparedMeals[req.DonationId]; exists {
+		delete(h.preparedMeals, req.DonationId)
+		return &pb.RollbackDeductMealStockResponse{
+			Success: true,
+			Message: "Meal rollback successfully",
+		}, nil
+	}
+
+	return &pb.RollbackDeductMealStockResponse{
+		Success: false,
+		Message: "No prepared meal found for this donation",
 	}, nil
 }
