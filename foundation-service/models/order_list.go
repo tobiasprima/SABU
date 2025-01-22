540b1280e54e81ba3ebaecd78347bc9f4a966b47
Dominus39 <yugoalexander@gmail.com>
2025-01-22 16:27:07 +0700

    change id to uuid

--- foundation-service/models/order_list.go
+++ foundation-service/models/order_list.go
@@ -1,7 +1,7 @@
 package models
 
 type OrderList struct {
-	ID           string     `json:"id" gorm:"primarykey;autoIncrement"`
+	ID           string     `json:"id" gorm:"column:id;type:uuid;default:gen_random_uuid();primaryKey"`
 	FoundationID string     `json:"foundation_id" gorm:"not null"`
 	Foundation   Foundation `json:"-" gorm:"foreignkey:FoundationID"`
 	Status       string     `json:"status"`
